4b94089caf697fc8a57f34fd09d0064501e5c0e6
quentin lourenco <quentin.lourenco9@gmail.com>
2022-03-16 18:08:53 +0100

    fix navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,12 +27,3 @@ import "bootstrap";
 
 
 // Internal imports
-
-// const currentLocation = location.href;
-// const menuItem = document.querySelectorAll('.nav-item');
-// const menuLength = menuItem.length
-// for (let i = 0; i < menuLength; i++) {
-//   if (menuItem[i].href === currentLocation) {
-//     menuItem[i].className = "active"
-//   }
-// }
