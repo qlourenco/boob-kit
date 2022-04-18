02299f1596d6191e3757739aa9fd608ef8b5c42d
quentin lourenco <quentin.lourenco9@gmail.com>
2022-04-18 12:32:55 +0200

    Change name routes, controller, view 'projet' for 'livret'

--- app/controllers/livrets_controller.rb
+++ app/controllers/livrets_controller.rb
@@ -1,4 +1,4 @@
-class ProjetsController < ApplicationController
+class LivretsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index ]
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "equipe", to: "equipes#index"
-  get "projet", to: "projets#index"
+  get "livret", to: "livrets#index"
 
   resources :forums, only: [:index, :new, :create, :destroy]
 end
